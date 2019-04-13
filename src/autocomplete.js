f65303fa66fedebbd17ecce4e6d36e69cddabc5a
fzahid1 <fzahid3098@gmail.com>
2019-04-13 15:02:52 -0400

    Update autocomplete.js

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -14,14 +14,44 @@ function autocomplete(inp) {
 		this.parentNode.appendChild(a);
 		
 		getSearches(searchVal);
+		for(i = 0; i < bestMatches.length; i++){
+			b = document.createElement("DIV");
+			b.innerHTML = "<strong>" + bestMatches[i].substr(0, searchVal.length) + "</strong>";
+			b.innerHTML += "<input type= 'hidden' value= '" + bestMatches[i] + "'>";
 
+			b.addEventListener("click", function(e){
+
+				inp.value = this.getElementsByTagName("input")[0].value;
+				closeAllLists();
+			});
+			a.appendChild(b);
+		}
 
 	});
+	inp.addEventListener("keydown", function(e){
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if(x) x = x.getElementsByTagName("div");
+		if(e.keyCode == 40){
+			currentFocus++;
+			addActive(x);
+		}else if(e.keyCode == 38){
+			currentFocus--;
+			addActive(x);
+		}else if(e.keyCode == 13){
+			e.preventDefault();
+			if(currentFocus > -1){
+				if(x) x[currentFocus].click();
+			}
+		}
+	});
 
-
-
-
-}
+	function addActive(x){
+		if(!x) return false;
+		removeActive(x);
+		if(currentFocus >= x.length) currentFocus = 0;
+		if(currentFocus < 0) currentFocus = (x.length - 1);
+		x[currentFocus].classList.add("autocomplete-active");
+	}
 
 function getSearches(searchVal){
 
@@ -35,10 +65,10 @@ function getSearches(searchVal){
 		console.log(data);
 
 		for(i = 0; i < data.bestMatches.length; i++){
-			console.log(data.bestMatches[i]); 
+			data.bestMatches[i]["2. name"] 
 
 		}
 	})
 
-	
+
 }
