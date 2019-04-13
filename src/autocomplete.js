4fcc4174576f07036f464e1e96aac90084dc3dcd
Ashish Bachavala <ashish0810@yahoo.com>
2019-04-13 18:13:43 -0400

    Autofill

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,24 +1,109 @@
-function updateSuggestions() {
-	var q = document.getElementById("searchBar").value;
-	console.log(q);
-	closeAllLists();
-	var a = document.getElementById("autocomplete-list");
-	getSearches(q, a);
+// function updateSuggestions() {
+// 	var q = document.getElementById("searchBar").value;
+// 	console.log(q);
+// 	if (q == "") {
+// 		return false;
+// 	}
+// 	closeAllLists();
+// 	var a = document.getElementById("autocomplete-list");
+// 	getSearches(q, a);
+// }
+
+// function closeAllLists() {
+// 	document.getElementById("autocomplete-list").innerHTML = "";
+// 	// var x = document.getElementsByClassName("autocomplete-items");
+// 	// for (var i = 0; i < x.length; i++) {
+// 	// 	if (elmnt != x[i]) {
+// 	// 		x[i].parentNode.removeChild(x[i]);
+// 	// 	}
+// 	// }
+// }
+
+function autocomplete(inp) {
+	/*the autocomplete function takes two arguments,
+	the text field element and an array of possible autocompleted values:*/
+	var currentFocus;
+	/*execute a function when someone writes in the text field:*/
+	inp.addEventListener("input", function(e) {
+		var a, b, i, val = this.value;
+		/*close any already open lists of autocompleted values*/
+		closeAllLists();
+		if (!val || val.length > 5) { return false;}
+		currentFocus = -1;
+		/*create a DIV element that will contain the items (values):*/
+		a = document.createElement("DIV");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+		/*append the DIV element as a child of the autocomplete container:*/
+		this.parentNode.appendChild(a);
+		/*for each item in the array...*/
+		getSearches(val, inp, a);
+	});
+
+	/*execute a function presses a key on the keyboard:*/
+	inp.addEventListener("keydown", function(e) {
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if (x) x = x.getElementsByTagName("div");
+		if (e.keyCode == 40) {
+			/*If the arrow DOWN key is pressed,
+			increase the currentFocus variable:*/
+			currentFocus++;
+			/*and and make the current item more visible:*/
+			addActive(x);
+		} else if (e.keyCode == 38) { //up
+			/*If the arrow UP key is pressed,
+			decrease the currentFocus variable:*/
+			currentFocus--;
+			/*and and make the current item more visible:*/
+			addActive(x);
+		} else if (e.keyCode == 13) {
+			/*If the ENTER key is pressed, prevent the form from being submitted,*/
+			e.preventDefault();
+			if (currentFocus > -1) {
+				/*and simulate a click on the "active" item:*/
+				if (x) x[currentFocus].click();
+			}
+		}
+	});
+
+	function addActive(x) {
+		/*a function to classify an item as "active":*/
+		if (!x) return false;
+		/*start by removing the "active" class on all items:*/
+		removeActive(x);
+		if (currentFocus >= x.length) currentFocus = 0;
+		if (currentFocus < 0) currentFocus = (x.length - 1);
+		/*add class "autocomplete-active":*/
+		x[currentFocus].classList.add("autocomplete-active");
+	}
+
+	function removeActive(x) {
+		/*a function to remove the "active" class from all autocomplete items:*/
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("autocomplete-active");
+		}
+	}
+
+	function closeAllLists(elmnt) {
+		/*close all autocomplete lists in the document,
+		except the one passed as an argument:*/
+		var x = document.getElementsByClassName("autocomplete-items");
+		for (var i = 0; i < x.length; i++) {
+			if (elmnt != x[i] && elmnt != inp) {
+				x[i].parentNode.removeChild(x[i]);
+			}
+		}
+	}
+
+	/*execute a function when someone clicks in the document:*/
+	document.addEventListener("click", function (e) {
+		closeAllLists(e.target);
+	});
 }
 
-function closeAllLists() {
-	document.getElementById("autocomplete-list").innerHTML = "";
-	// var x = document.getElementsByClassName("autocomplete-items");
-	// for (var i = 0; i < x.length; i++) {
-	// 	if (elmnt != x[i]) {
-	// 		x[i].parentNode.removeChild(x[i]);
-	// 	}
-	// }
-}
-
-function getSearches(searchVal, a) {
+function getSearches(searchVal, inp, a) {
 	var url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + searchVal + '&apikey=KYJ63OHXTZUJ9Q4I'
-
+	var b;
 	fetch(url)
 	.then((resp) => resp.json())
 	.then(function(data) {
@@ -26,13 +111,15 @@ function getSearches(searchVal, a) {
 		console.log(data);
 		for(i = 0; i < data.bestMatches.length; i++){
 			console.log(data.bestMatches[i]["2. name"]);
-			var b = document.createElement("DIV");
-			b.innerHTML += data.bestMatches[i]["2. name"];
+			/*create a DIV element for each matching element:*/
+			b = document.createElement("DIV");
+			b.innerHTML += data.bestMatches[i]["2. name"] + " (" + data.bestMatches[i]["1. symbol"] + ")";
+			/*insert a input field that will hold the current array item's value:*/
 			b.innerHTML += "<input type='hidden' value='" + data.bestMatches[i]["2. name"] + "'>";
-			b.setAttribute("class", "autocomplete-items");
-			b.addEventListener("click", function(e) {
+			/*execute a function when someone clicks on the item value (DIV element):*/
+				b.addEventListener("click", function(e) {
 				/*insert the value for the autocomplete text field:*/
-				document.getElementById("searchBar").value = this.getElementsByTagName("input")[0].value;
+				inp.value = this.getElementsByTagName("input")[0].value;
 				/*close the list of autocompleted values,
 				(or any other open lists of autocompleted values:*/
 				closeAllLists();
